f185b748f363fcb8698912662419f9d50c550870
Mario Blazquez <mario.blazquez.m@gmail.com>
2022-08-18 18:19:31 +0200

    Iteration 2 complete

--- first-project/src/components/ToDoItem.vue
+++ first-project/src/components/ToDoItem.vue
@@ -1,15 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('nice');
+</script>
+
 <template>
   <div>
-    <label for="todo-item"><input type="checkbox" id="todo-item" />My Todo Item</label>
+    <label for="todo-item"
+      ><input type="checkbox" id="todo-item" />My Todo Item {{ msg }}</label
+    >
   </div>
 </template>
 
-<script>
-export default {
-
-};
-</script>
-
 <style>
-
 </style>

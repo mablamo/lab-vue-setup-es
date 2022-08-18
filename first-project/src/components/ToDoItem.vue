b5f8071f4be5e5b758a4a6bc554efea5e3fe8034
Mario Blazquez <mario.blazquez.m@gmail.com>
2022-08-18 20:12:38 +0200

    Iteration 3 complete

--- first-project/src/components/ToDoItem.vue
+++ first-project/src/components/ToDoItem.vue
@@ -2,13 +2,15 @@
 import { ref } from 'vue';
 
 const msg = ref('nice');
+const helloWorldFnc = () => {
+  console.log('Hello World!');
+};
 </script>
 
 <template>
   <div>
-    <label for="todo-item"
-      ><input type="checkbox" id="todo-item" />My Todo Item {{ msg }}</label
-    >
+    <label for="todo-item"><input type="checkbox" id="todo-item" />My Todo Item {{ msg }}</label>
+    <button @click="helloWorldFnc">Hello World!</button>
   </div>
 </template>
 
